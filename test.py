ba3642c0148d4a18667ad15c59077bdfe29d46c7
bode <40768803+bode135@users.noreply.github.com>
2022-05-09 16:09:20 +0800

    Update test.py

--- test.py
+++ test.py
@@ -1,23 +1,60 @@
-from pydamo_0 import Time, vk, DM, Key, Mouse
+from pydamo_0 import Time, DM, Mouse, Key, vk
 
-dm = DM()           # 初始化
-# dm.reg()           # 注册
+
+dm = DM()
+# dm. reg()     # 后台按键需要注册
+# dm.reg_infos.unreg_dm()
 
 ms = Mouse(dm)
 kk = Key(dm)
 tt = Time()
 
-dm.Beep()    # 蜂鸣器
+dm.Beep()       # 蜂鸣器
 
-print('your mouse\'s position: ', ms.position)
-tt.sleep(1)
+
+# ---------------- mouse simulation ---------------
+ms.position     # 当前鼠标位置
 
 x, y = (1000, 500)
-ms.position = x, y
-print('move your mouse\'s to: ', ms.position)
+ms.move_to(x, y)                    # 移动鼠标_1
 tt.sleep(1)
+x, y = (0, 0)
+ms.position = x, y                  # 移动鼠标_2
 
 
-ms.click_right(10, 10)
-print('clike mouse_right: ', ms.position)
+tt.sleep(1)
+ms.click_right(x, y)                # 右键单击(x,y)位置, 按下抬起的间隔为0.5s
 
+tt.sleep(1)
+x, y = (1000, 500)
+ms.click_left(x, y, 2)              # 左键单击(x,y)位置, 默认按下抬起的间隔为2s
+
+# ------------ key simulation -----------------------
+tt.sleep(1)
+kk.dp('a')                          # 按下a键
+tt.sleep(1)
+kk.dp(vk.enter, 1)                  # 测试enter键
+
+# 全选操作
+tt.sleep(1)
+kk.down(vk.ctrl)
+kk.down('a')
+kk.up(vk.ctrl)
+kk.up('a')
+
+# 像素颜色捕捉
+def conv_to_rgb(color):
+    RGB_str = [color[:2], color[2:-2], color[-2:]]
+    RGB = [int(i, 16) for i in RGB_str]
+    return RGB
+
+tt.__init__()
+while (tt.during(10)):  # 10s内捕捉鼠标当前位置的颜色
+    tt.sleep(0.1)
+
+    if (tt.get_key_state(vk.mouse_right)):   break  # 按下鼠标右键则退出
+
+    x, y = ms.position
+    color = dm.GetColor(x, y)
+
+    print(tt.now(1), color, '鼠标位置颜色RGB值:', conv_to_rgb(color))

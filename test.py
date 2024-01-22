5329b66cf71650abf3e79d48ba7123eab0caec30
bode135 <2248270222@qq.com>
2024-01-22 11:52:01 +0800

    bdtime的更新__stop和get_key_state

--- test.py
+++ test.py
@@ -16,39 +16,39 @@ tt = Time()
 
 dm.Beep()  # 蜂鸣器
 
-# ---------------- mouse simulation ---------------
-print(f'Mouse position: {ms.position}')  # 当前鼠标位置
-
-tt.sleep(1)
-x, y = (1000, 500)
-ms.move_to(x, y)  # 移动鼠标_1
-
-tt.sleep(1)
-x, y = (0, 0)
-ms.position = x, y  # 移动鼠标_2
-
-xy = (-100, 0)
-dm.MoveR(*xy)
-
-tt.sleep(1)
-ms.click_right(x, y)  # 右键单击(x,y)位置, 按下抬起的间隔为0.5s
-
-tt.sleep(1)
-x, y = (1000, 500)
-ms.click_left(x, y, 2)  # 左键单击(x,y)位置, 默认按下抬起的间隔为2s
-
-# ------------ key simulation -----------------------
-tt.sleep(1)
-kk.dp('a')  # 按下a键
-tt.sleep(1)
-kk.dp(vk.enter, 1)  # 测试enter键
-
-# 全选操作
-tt.sleep(1)
-kk.down(vk.ctrl)
-kk.down('a')
-kk.up(vk.ctrl)
-kk.up('a')
+# # ---------------- mouse simulation ---------------
+# print(f'Mouse position: {ms.position}')  # 当前鼠标位置
+#
+# tt.sleep(1)
+# x, y = (1000, 500)
+# ms.move_to(x, y)  # 移动鼠标_1
+#
+# tt.sleep(1)
+# x, y = (0, 0)
+# ms.position = x, y  # 移动鼠标_2
+#
+# xy = (-100, 0)
+# dm.MoveR(*xy)
+#
+# tt.sleep(1)
+# ms.click_right(x, y)  # 右键单击(x,y)位置, 按下抬起的间隔为0.5s
+#
+# tt.sleep(1)
+# x, y = (1000, 500)
+# ms.click_left(x, y, 2)  # 左键单击(x,y)位置, 默认按下抬起的间隔为2s
+#
+# # ------------ key simulation -----------------------
+# tt.sleep(1)
+# kk.dp('a')  # 按下a键
+# tt.sleep(1)
+# kk.dp(vk.enter, 1)  # 测试enter键
+#
+# # 全选操作
+# tt.sleep(1)
+# kk.down(vk.ctrl)
+# kk.down('a')
+# kk.up(vk.ctrl)
+# kk.up('a')
 
 
 # 像素颜色捕捉
@@ -62,8 +62,11 @@ tt.__init__()
 while tt.during(10):  # 10s内捕捉鼠标当前位置的颜色
     tt.sleep(0.1)
 
-    if tt.get_key_state(vk.mouse_right):
-        break  # 按下鼠标右键则退出
+    if tt.get_key_state(vk.mouse_right) or tt.is_pressed('alt + x'):
+        print('--- stopped!')
+        break  # 按下鼠标右键, 或者`alt + x`, 则退出循环
+
+    # tt.stop_alt('s')  # 按下`alt + s`则停止进程
 
     x, y = ms.position
     color = dm.GetColor(x, y)
